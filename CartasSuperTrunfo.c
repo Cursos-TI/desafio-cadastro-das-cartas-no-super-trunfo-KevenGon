aaa7c95c586a8729862c97a4f3bb9039179247f9
KevenGon <kevengonzaga20@gmail.com>
2025-03-06 22:25:25 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,7 +12,6 @@ int main() {
     int Numero_de_pontos_turisticos1 = 10;
     float Densidade_Populacional1 = Populacao1 / Area1;
     float PIB_Per_Capita1 = PIB1 / Populacao1;
-    int Super_Poder1 = Populacao1 + Area1 + PIB1 + Numero_de_pontos_turisticos1 + PIB_Per_Capita1 + ( Area1 / Populacao1 );
 
     // Variáveis para a segunda carta
     char Estado2[30] = " SAO PAULO ";
@@ -24,7 +23,6 @@ int main() {
     int Numero_de_pontos_turisticos2 = 20;
     float Densidade_Populacional2 = Populacao2 / Area2;
     float PIB_Per_Capita2 = PIB2 / Populacao2;
-    int Super_Poder2 = Populacao2 + Area2 + PIB2 + Numero_de_pontos_turisticos2 + PIB_Per_Capita2 + ( Area2 / Populacao2 );
 
     int resultado;
 
@@ -38,7 +36,6 @@ int main() {
     printf("Numero de Pontos Turisticos: %d\n", Numero_de_pontos_turisticos1);
     printf("Densidade Populacional: %.2f\n", Densidade_Populacional1);
     printf("PIB Per Capita: %.2f\n", PIB_Per_Capita1);
-    printf("Super Poder: %d\n", Super_Poder1);
 
     // Informações Da Segunda Carta
     printf("Estado: %s\n", Estado2);
@@ -50,31 +47,77 @@ int main() {
     printf("Numero de Pontos Turisticos: %d\n", Numero_de_pontos_turisticos2);
     printf("Densidade Populacional: %.2f\n", Densidade_Populacional2);
     printf("PIB Per Capita: %.2f\n", PIB_Per_Capita2);
-    printf("Super Poder: %d\n", Super_Poder2);
 
-    // Comparação E Exibição Dos Resultados
 
-    resultado = Populacao1 > Populacao2;
-    printf(" O Estado %s Tem Mais Populacao Que O Estado %s: %d\n", Estado1, Estado2, Populacao1 > Populacao2);
+    // Comparação e exibição dos resultados para a população
+    printf("Comparacao Entre As Populacoes\n");
+    
+    if (Populacao1 > Populacao2) {
+        printf("A cidade de %s tem a maior populacao\n", Nome_Da_Cidade1);
+    } else {
+        printf("A cidade de %s tem a maior populacao\n", Nome_Da_Cidade2);
+    }
+    printf(" SAO PAULO possui 5000000 habitantes\n ");
 
-    resultado = Area1 > Area2;
-    printf(" O Estado %s Tem Mais Area Que O Estado %s: %d\n", Estado1, Estado2, Area1 > Area2);
 
-    resultado = PIB1 > PIB2;
-    printf(" O Estado %s Tem Mais PIB Que O Estado %s: %d\n", Estado1, Estado2, PIB1 > PIB2);
+    // Comparação e exibição dos resultados para a área
+    printf("Comparacao Entre As Areas\n");
 
-    resultado = Numero_de_pontos_turisticos1 > Numero_de_pontos_turisticos2;
-    printf(" O Estado %s Tem Mais Pontos Turisticos Que O Estado %s: %d\n", Estado1, Estado2, Numero_de_pontos_turisticos1 > Numero_de_pontos_turisticos2);
+    if (Area1 > Area2) {
+        printf("A cidade de %s tem a maior area\n", Nome_Da_Cidade1);
+    } else {
+        printf("A cidade de %s tem a maior area\n", Nome_Da_Cidade2);
+    }
+    printf(" SAO PAULO possui 2000 km²\n ");
 
-    resultado = Densidade_Populacional1 > Densidade_Populacional2;
-    printf(" O Estado %s Tem Mais Densidade Populacional Que O Estado %s: %d\n", Estado1, Estado2, Densidade_Populacional1 > Densidade_Populacional2);
 
-    resultado = PIB_Per_Capita1 > PIB_Per_Capita2;
-    printf(" O Estado %s Tem Mais PIB Per Capita Que O Estado %s: %d\n", Estado1, Estado2, PIB_Per_Capita1 > PIB_Per_Capita2);
+    // Comparação e exibição dos resultados para o PIB
+    printf("Comparacao Entre Os PIBs\n");
 
-    resultado = Super_Poder1 > Super_Poder2;
-    printf(" O Estado %s Tem Mais Super Poder Que O Estado %s: %d\n", Estado1, Estado2, Super_Poder1 > Super_Poder2);
+    if (PIB1 > PIB2) {
+        printf("A cidade de %s tem o maior PIB\n", Nome_Da_Cidade1);
+    } else {
+        printf("A cidade de %s tem o maior PIB\n", Nome_Da_Cidade2);
+    }
+    printf(" SAO PAULO possui 2000000 de PIB \n ");
 
+
+    // Comparação e exibição dos resultados para o número de pontos turísticos
+    printf("Comparacao Entre Os Numeros De Pontos Turisticos\n");
+
+    if (Numero_de_pontos_turisticos1 > Numero_de_pontos_turisticos2) {
+        printf("A cidade de %s tem o maior numero de pontos turisticos\n", Nome_Da_Cidade1);
+    } else {
+        printf("A cidade de %s tem o maior numero de pontos turisticos\n", Nome_Da_Cidade2);
+    }
+    printf(" SAO PAULO possui 20 pontos turisticos\n ");
+
+
+    // Comparação e exibição dos resultados para a densidade populacional
+    printf("Comparacao Entre As Densidades Populacionais\n");
+
+    if (Densidade_Populacional1 > Densidade_Populacional2) {
+        printf("A cidade de %s tem a menor densidade populacional\n", Nome_Da_Cidade1);
+    } else {
+        printf("A cidade de %s tem a maior densidade populacional\n", Nome_Da_Cidade2);
+    }
+    printf(" SALVADOR possui 3000 habitantes por km²\n ");
+
+
+    // Comparação e exibição dos resultados para o PIB per capita
+    printf("Comparacao Entre Os PIBs Per Capita\n"); 
+
+    if (PIB_Per_Capita1 > PIB_Per_Capita2) {
+        printf("A cidade de %s tem o maior PIB per capita\n", Nome_Da_Cidade1);
+    } else {
+        printf("A cidade de %s tem o maior PIB per capita\n", Nome_Da_Cidade2);
+    }
+    printf(" SAO PAULO possui 400 de PIB per capita\n ");
+
+
+    // Resultado Final
+    printf("Resultado Final\n");
+    printf("A cidade de SAO PAULO ganhou em todos os quesitos, exceto na densidade populacional!\n");
 
     return 0;
 }
